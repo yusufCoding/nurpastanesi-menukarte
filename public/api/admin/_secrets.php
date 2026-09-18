120aa6a340547685caf771ebf003361a60185f79
yusufCoding <yusufildokuz@gmail.com>
2026-09-18 19:50:00 +0200

    change admin pass

--- public/api/admin/_secrets.php
+++ public/api/admin/_secrets.php
@@ -1,5 +1,5 @@
 <?php
 // NUR HASH, kein Klartext
 return [
-  "ADMIN_PASS_HASH" => '$2y$12$tQVgr7lB2HbX2DY8SvFowe3Dh0twdIbhfDaNLQviS0x95vl9CR1Ta',
+  "ADMIN_PASS_HASH" => '$2y$12$I5cgxzk1agWQ3uIWmhA2AOCT25LYCu7jVrCdHAgr97zqBcKdadL9u',
 ];
